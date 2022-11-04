a350a28251714d165a6946f2f2a8e5c42c42f7a4
Daniel Szydlowski <dszydlowski@wisc.edu>
2022-11-03 21:23:05 -0500

    finish homework

--- generate_clustered_pt_proc.R
+++ generate_clustered_pt_proc.R
@@ -19,6 +19,11 @@ Ymax=50
 centers=matrix(data=c(-25,-25,25,25,-25,25,-25,25),nrow=4,ncol=2)
 dist=matrix(nrow=4,ncol=1)
 
+centers=matrix(data=c(0,0,0,0,0,0,0,0),nrow=4,ncol=2)
+
+# can also change centers to influence the ripley's K plot
+
+
 #define outputs
 output.X=matrix(nrow = Pointnum, ncol = 1)
 output.Y=matrix(nrow = Pointnum, ncol = 1)
@@ -69,3 +74,114 @@ points(centers,type="p", col="red", pch=21, bg="red")
 
 #create a point pattern object for analysis using the spatstat library
 output_ppp = ppp(output.X, output.Y, c(Xmin,Xmax), c(Ymin,Ymax))
+
+##### Question 1.3 ##### Randomly generate data
+
+output.X.rand = runif(100, Xmin, Xmax)
+output.Y.rand = runif(100, Ymin, Ymax)
+
+plot(output.X.rand,output.Y.rand)
+
+# create a ppp object for the random data
+
+pppRand  = ppp(output.X.rand, output.Y.rand, c(Xmin,Xmax), c(Ymin,Ymax))
+
+quadrat.test(output_ppp)
+quadrat.test(pppRand)
+
+# calculate and plot Ripley's K
+
+
+randEnv = envelope(pppRand)
+
+plot(Kest(output_ppp))
+plot(Kest(pppRand))
+
+
+
+
+
+#### Running with center at 0, 0
+
+centers=matrix(data=c(0,0,0,0,0,0,0,0),nrow=4,ncol=2)
+
+
+dist=matrix(nrow=4,ncol=1)
+
+centers=matrix(data=c(0,0,0,0,0,0,0,0),nrow=4,ncol=2)
+
+# can also change centers to influence the ripley's K plot
+
+
+#define outputs
+output.X=matrix(nrow = Pointnum, ncol = 1)
+output.Y=matrix(nrow = Pointnum, ncol = 1)
+
+#precalcs - calculate the slope of the clustering effect
+slope=(val.at.center-background)/effect.range
+
+#set a counter
+outcounter=0
+
+#main for loop
+for (i in 1:100000){
+  #generate a random candidate point
+  x.candidate=runif(1, min=Xmin, max=Xmax)
+  y.candidate=runif(1, min=Ymin, max=Ymax)
+  
+  #calculate the distance between the candidate point and the nearest cluster center
+  for (j in 1:4){
+    dist[j]=sqrt((x.candidate-centers[j,1])^2+(y.candidate-centers[j,2])^2)
+  }
+  min.dist=min(dist)
+  
+  #calculate the probability of retaining the candidate point
+  if(min.dist<effect.range){
+    prob=val.at.center-slope*min.dist
+  }
+  else
+    prob=background
+  
+  #roll the dice to see if you keep the candidate point
+  testval=runif(1,min=0,max=1)  
+  if (testval<prob){
+    outcounter=outcounter+1
+    keep=1
+    output.X[outcounter]=x.candidate
+    output.Y[outcounter]=y.candidate
+  }
+  
+  #if you've reached your target number of points, break from the for loop
+  if(outcounter==Pointnum){
+    break
+  }
+}
+
+#plot the points and the cluster centers
+plot(output.X,output.Y)
+points(centers,type="p", col="red", pch=21, bg="red")
+
+#create a point pattern object for analysis using the spatstat library
+output_pppCenter = ppp(output.X, output.Y, c(Xmin,Xmax), c(Ymin,Ymax))
+
+##### Question 1.3 ##### Randomly generate data
+
+output.X.rand = runif(100, Xmin, Xmax)
+output.Y.rand = runif(100, Ymin, Ymax)
+
+plot(output.X.rand,output.Y.rand)
+
+# create a ppp object for the random data
+
+pppRand  = ppp(output.X.rand, output.Y.rand, c(Xmin,Xmax), c(Ymin,Ymax))
+
+quadrat.test(output_ppp)
+quadrat.test(pppRand)
+
+# calculate and plot Ripley's K
+
+
+randEnv = envelope(pppRand)
+
+plot(Kest(output_pppCenter))
+plot(Kest(pppRand))
